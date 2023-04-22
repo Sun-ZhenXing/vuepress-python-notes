d9fb1ac0fc433c5080547116591e913760540e74
Sun-ZhenXing <1006925066@qq.com>
2023-04-22 14:49:58 +0800

    chore: update config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,9 @@ import { searchProPlugin } from 'vuepress-plugin-search-pro'
 import { autoCatalogPlugin } from 'vuepress-plugin-auto-catalog'
 
 const __dirname = getDirname(import.meta.url)
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
+const ROOT_PATH = path.resolve(__dirname, '../..')
+const CURRENT_PATH = path.resolve(__dirname, '.')
 const USER_NAME = 'Sun-ZhenXing'
 const BASE_PATH = '/vuepress-python-notes/'
 
@@ -18,25 +20,45 @@ export default defineUserConfig({
   head: [
     ['link', { rel: 'icon', href: `${BASE_PATH}favicon.svg` }]
   ],
-  base: BASE_PATH,
   markdown: {
     code: {
       lineNumbers: 10
-    }
+    },
+    importCode: {
+      handleImportPath: str => str
+        .replace(/^\//, ROOT_PATH.replace(/(?:|\\|\/)$/, '/'))
+        .replace(/^@/, CURRENT_PATH),
+    },
   },
   theme: defaultTheme({
     logo: '/favicon.svg',
     repo: `${USER_NAME}${BASE_PATH}`,
     docsDir: 'docs',
+    selectLanguageName: '简体中文',
+    selectLanguageText: '选择语言',
+    selectLanguageAriaLabel: '选择语言',
     editLinkText: '在 GitHub 上编辑此页',
     contributorsText: '贡献者',
     lastUpdatedText: '上次更新',
     openInNewWindow: '在新窗口打开',
-    toggleColorMode: '切换深色模式',
+    toggleColorMode: '切换颜色模式',
     toggleSidebar: '切换侧边栏',
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
+    backToHome: '返回首页',
     navbar: [
     ],
     sidebar: 'auto',
+    themePlugins: {
+      git: isProd,
+    },
   }),
   plugins: [
     mdEnhancePlugin({
@@ -45,7 +67,15 @@ export default defineUserConfig({
       linkCheck: true,
       vPre: true,
       tabs: true,
+      card: true,
       codetabs: true,
+      include: {
+        resolvePath: file => {
+          if (file.startsWith('@'))
+            return file.replace('@', CURRENT_PATH)
+          return file
+        },
+      },
       align: true,
       attrs: true,
       sub: true,
@@ -53,7 +83,6 @@ export default defineUserConfig({
       footnote: true,
       mark: true,
       imgLazyload: true,
-      include: true,
       tasklist: true,
       katex: true,
       mermaid: true,
@@ -103,13 +132,10 @@ export default defineUserConfig({
       }
     }),
     copyCodePlugin({
-      showInMobile: true
+      showInMobile: true,
     }),
   ],
   alias: {
-    '@': path.resolve(
-      __dirname,
-      '.',
-    )
+    '@': CURRENT_PATH,
   },
 })
